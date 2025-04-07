bc2554fff180b1002d41cc88152af51b620fead5
Dan <milocodes.io@gmail.com>
2025-04-07 20:30:53 +0100

    sidebar complete

--- js/main.js
+++ js/main.js
@@ -15,26 +15,19 @@ $(document).ready(function () {
 
 // Function to reveal side menu
 
-/* 
-- Animate Hamburger
-- Move #main-content left 
-- Add overlay to #main-content when sidemenu is active
-*/
-
 function clickEvent(event) {
-    let target = $(event.target);
+    let $target = $(event.target);
     const $mainContent = $('#main-content');
-    const $hamburger = $('hamburger-btn');
+    const $hamburger = $('.hamburger-btn');
     const $sideMenu = $('#side-menu')
 
     // Conditional to check if #main-content has active, if so then check if click was registered outside of #side-menu
 
-
-    if (target.closest('.hamburger-btn').length && !$hamburger.hasClass('active')) {
-        $hamburger.toggleClass('active');
-        $mainContent.toggleClass('active');
+    if ($target.is($hamburger) && !$hamburger.hasClass('active')) {
+        $hamburger.addClass('active');
+        $mainContent.addClass('active');
     } else
-        if (!target.is($sideMenu) && $mainContent.hasClass('active')) {
+        if (!$sideMenu.has($target).length && $mainContent.hasClass('active') && $hamburger.hasClass('active')) {
             $mainContent.removeClass('active');
             $hamburger.removeClass('active');
         }
@@ -42,3 +35,18 @@ function clickEvent(event) {
 }
 
 $('body').on('click', clickEvent);
+
+
+// Function to track user scroll and reveal sticky header
+let lastKnownScrollPosition = 0;
+document.addEventListener("scroll", (event) => {
+    // If scrollY > 
+    if (window.scrollY > lastKnownScrollPosition) {
+        console.log("User scrolled down");
+    } else {
+        console.log("User scrolled up");
+    }
+    lastKnownScrollPosition = window.scrollY;
+    console.log("event", event);
+    console.log("lastKnownScrollPosition", lastKnownScrollPosition);
+})
