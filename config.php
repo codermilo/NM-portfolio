31713f9790217537b7e7b25bcdc8084d48efde0d
Dan <milocodes.io@gmail.com>
2025-05-28 14:24:45 +0100

    stashing

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Load .env file from the project root
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 return [
     'database' => [
