ee36714a5ad670c5930085ea91618ee25c2c9e09
Dan <milocodes.io@gmail.com>
2025-05-28 14:31:31 +0100

    working locally

--- config.php
+++ config.php
@@ -1,17 +1,19 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Load .env file from the project root
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
 $dotenv->load();
 
 return [
     'database' => [
-        'host' => '127.0.0.1',
-        'port' => 3306,
-        'dbname' => 'nmhomepage',
-        'charset' => 'utf8mb4'
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
+        'dbname' => $_ENV['DB_DATABASE'],
+        'charset' => 'utf8mb4',
+        'username' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD']
     ],
 
     'services' => [
